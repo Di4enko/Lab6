fb1e8ab405f1a7801b54e567be2370d3d8ab57cf
Dima Reznichenko <90556507+Di4enko@users.noreply.github.com>
2022-02-09 11:06:00 +0300

    add source code

---
+++ src/com/Field.java
@@ -0,0 +1,48 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Field extends JPanel {
+    private boolean paused;
+    private List<BouncingBall> balls = new ArrayList<BouncingBall>(10);
+    private Timer repaintTimer = new Timer(10, e -> repaint()) ;
+
+    public Field(){
+        setBackground(Color.WHITE);
+        repaintTimer.start();
+    }
+
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
+        Graphics2D canvas = (Graphics2D) graphics;
+        for(BouncingBall ball: balls){
+            ball.paint(canvas);
+        }
+    }
+
+    public  void addBall(){
+        balls.add(new BouncingBall(this));
+    }
+
+    public synchronized void pause(){
+        paused = true;
+    }
+
+    public synchronized  void resume(){
+        paused=false;
+        notifyAll();
+    }
+
+    public synchronized  void canMove(BouncingBall ball) throws InterruptedException{
+        if(paused){
+            wait();
+        }
+    }
+
+    public List<BouncingBall> getBalls() {
+        return balls;
+    }
+}
---
+++ src/com/MainFrame.java
@@ -0,0 +1,61 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+
+    private JMenuItem pauseMenuItem;
+    private JMenuItem resumeMenuItem;
+
+    private Field field = new Field();
+
+    public MainFrame(){
+        super("Программирование и синхронизация потоков");
+        setSize(WIDTH,HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width-WIDTH)/2,
+                    (kit.getScreenSize().height-HEIGHT)/2);
+        setExtendedState(MAXIMIZED_BOTH);
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu ballMenu = new JMenu("Мячи");
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                field.addBall();
+                if(!pauseMenuItem.isEnabled() && !resumeMenuItem.isEnabled()){
+                    pauseMenuItem.setEnabled(true);
+                }
+            }
+        };
+        menuBar.add(ballMenu);
+        ballMenu.add(addBallAction);
+        JMenu controlMenu = new JMenu("Управление");
+        menuBar.add(controlMenu);
+        Action pauseAction = new AbstractAction("Приостановить движение") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
+        getContentPane().add(field, BorderLayout.CENTER);
+    }
+}
